df9bc8dc4abe5975176400c5e9b081630e294b11
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-09-12 14:24:06 +0300

    Solving tasks for certification

---
+++ 01/conditionals.py
@@ -0,0 +1,40 @@
+'''
+Write a program that prompts the user for a string
+and checks whether the string is a palindrome (i.e., the string reads the same forward and backward).
+'''
+
+# def is_palindrome(text: str):
+#     return text == text[::-1]
+#
+#
+# user_input = input("Enter a string: ")
+#
+# print(is_palindrome(user_input))
+
+'''
+Write a program that takes in a list of integers 
+and returns the sum of all even numbers in the list.
+'''
+
+
+# def get_even_numbers(numbers: list):
+#     even_nums = [num for num in numbers if num % 2 == 0]
+#     return sum(even_nums)
+#
+#
+# print(get_even_numbers([2, 4, 7, 8]))
+
+'''
+Write a program that prompts the user for their age 
+and checks whether they are old enough to vote (i.e., 18 years old or older).
+'''
+
+# age = input("Enter your age: ")
+# try:
+#     age = int(age)
+#     if age >= 18:
+#         print('You are old enough to vote.')
+#     else:
+#         print('You are not old enough to vote.')
+# except ValueError:
+#     print("Invalid input. Please enter a valid age as a number.")
