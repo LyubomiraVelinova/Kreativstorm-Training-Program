c513c24be2ee063d410fff4eb062abbaa5fa06b8
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-09-13 13:33:47 +0300

    Solving tasks for certification

--- 01/error_handling.py
+++ 01/error_handling.py
@@ -25,7 +25,6 @@ Write a function that takes a filename as an argument, and attempts to open the 
 If the file is successfully opened, the function should return the contents of the file.
 '''
 
-
 # def read_file(filename):
 #     try:
 #         with open(filename, 'r') as file:
@@ -43,3 +42,68 @@ If the file is successfully opened, the function should return the contents of t
 Write a function that takes a list of strings as an argument, and returns a new list containing only the strings that can be successfully converted to a float. 
 Use a try-except block to catch any ValueError exceptions that may be raised when attempting to convert a string to a float.
 '''
+
+# def strings_to_float(numbers: list):
+#     result = []
+#     for char in numbers:
+#         try:
+#             result.append(float(char))
+#         except ValueError:
+#             pass
+#
+#     return result
+#
+#
+# user_input = input("Enter list of strings, separated by comma and space: ")
+# list_input = user_input.split(', ')
+# print(strings_to_float(list_input))
+
+
+'''
+Write a function that takes a list of dictionaries as an argument, and returns the value of a specified key from each dictionary. 
+Use a try-except block to catch any KeyError exceptions that may be raised when attempting to access a key that does not exist in a dictionary.
+'''
+
+# def get_values_from_dicts(dicts, key):
+#     values = []
+#     for d in dicts:
+#         try:
+#             value = d[key]
+#             values.append(value)
+#         except KeyError:
+#             pass
+#
+#     return values
+#
+#
+# dict_list = [
+#     {'name': 'Lyuba', 'age': 30},
+#     {'name': 'Brave', 'age': 25},
+#     {'name': 'Huston'},
+# ]
+#
+# key_to_extract = 'age'
+# print(get_values_from_dicts(dict_list, key_to_extract))
+
+
+'''
+Write a function that takes a list of integers as an argument, and returns the largest integer in the list. 
+Use a try-except block to catch any ValueError exceptions that may be raised when attempting to compare elements that are not integers.
+'''
+
+
+def get_largest_integer(numbers: list):
+    largest_int = None
+
+    for char in numbers:
+        try:
+            if largest_int is None or int(char) > largest_int:
+                largest_int = int(char)
+        except ValueError:
+            pass
+    return largest_int
+
+
+user_input = input("Enter a list of integers, separated with comma and space: ")
+list_input = user_input.split(', ')
+print(get_largest_integer(list_input))
